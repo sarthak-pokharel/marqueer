c137853170f2ecca900483f4c6b9fa2041684c68
Sarthak <sarthak.whenever@gmailc.om>
2022-09-21 18:34:24 +0545

    link gen process done

--- main.js
+++ main.js
@@ -2,34 +2,55 @@ import $ from 'jquery'
 import './style.css'
 
 
-function marqueeText(t,v){
-  $('#marquee .mtxt').html(t).attr('scrollamount',v);
+function marqueeText(t, v) {
+  $('#marquee .mtxt').html(t).attr('scrollamount', v);
 }
 
-$(async function(){
-
-
-
+function getData() {
   let txt = 'This text is going to be marqueed.';
-  txt = prompt("Enter anything: ") || txt;
-  let scroll = prompt("Enter scroll velocity: ",20);
+  let scroll;
+  let u = new URL(window.location.href);
+
+  if (!(() => u.searchParams.get('crisis'))()) {
+    txt = prompt("Enter anything: ") || txt;
+    scroll = prompt("Enter scroll velocity: ", 20);
+    location.href = (() => {
+      
+      u.searchParams.append("crisis", "no");
+      u.searchParams.append("text", txt);
+      u.searchParams.append("scroll", scroll+"");
+      return u;
+    })();
+  }else {
+    txt = u.searchParams.get('text');
+    scroll = +u.searchParams.get('scroll')
+  }
+  return [txt, scroll];
+}
+
+$(async function () {
+
+
+
+
 
 
   $("#app").html(`<div class="marquee" id="marquee"><marquee class="mtxt" scrollamount="20" ></marquee></div>`);
 
-  marqueeText(txt,scroll);
+  marqueeText(...getData());
 });
 
-window.ondblclick = ()=>{
+window.ondblclick = () => {
   openFullscreen($('html')[0]);
 }
 let paused = false;
-window.onclick = ()=>{
+window.onclick = () => {
+  let m = $('marquee')[0];
   paused = !paused;
-  if(!paused){
-    $('#marquee').stop();
-  }else {
-    $('#marquee')[0].start();
+  if (paused) {
+    m.stop();
+  } else {
+    m.start();
   }
 }
 
